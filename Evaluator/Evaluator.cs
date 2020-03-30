3de8332e2ccdb2761b53a3f96573613c54ce6df1
Alexander Batashev <alexbatashev@outlook.com>
2020-03-30 19:52:13 +0300

    Win Project

---
+++ Evaluator/Evaluator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MathNet.Symbolics;
+
+namespace Evaluator
+{
+    public class Evaluator
+    {
+        public static double Evaluate(double x, string expr)
+        {
+            var parsed = Infix.ParseOrThrow(expr);
+            var values = new Dictionary<String, FloatingPoint>();
+            values.Add("x", x);
+
+            var res = MathNet.Symbolics.Evaluate.Evaluate(values, parsed);
+            return res.RealValue;
+        }
+    }
+}
---
+++ Win10/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Evaluator;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace Win10
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var res = Evaluator.Evaluator.Evaluate(1.0, expression.Text);
+            Console.WriteLine(res);
+        }
+    }
+}
