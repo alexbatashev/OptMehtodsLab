78c27ed28d31b16b4484b0f6cbd50a56f00a09d9
Alexander Batashev <alexbatashev@outlook.com>
2020-03-30 14:22:15 +0300

    Use pre-built parser

---
+++ OptMethodsLab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using MathNet.Symbolics;
+
+namespace OptMethodsLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Expression: ");
+            var expr = Console.ReadLine();
+            var parsed = Infix.ParseOrThrow(expr);
+            var res = Evaluate.Evaluate(new Dictionary<String, FloatingPoint>(), parsed);
+            Console.WriteLine(res);
+        }
+    }
+}
